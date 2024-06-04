88e9719753dfbd0b3d520bb4c3811e543e934165
diffident016 <marvin.tagolimotjr13@gmail.com>
2024-06-05 00:58:42 +0800

    feat: added truck tracker

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,133 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import TrackerMap from "./TrackerMap";
+import { rdb } from "../../firebase";
+import { onValue, ref } from "firebase/database";
+import "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
+import "leaflet-routing-machine";
+
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
 
 function Dashboard() {
   const [map, setMap] = useState(null);
+  const [route, setRoute] = useState(null);
+  const [marker, setMarker] = useState([]);
+
+  const [trucks, setTrucks] = useReducer(
+    (prev, next) => {
+      return { ...prev, ...next };
+    },
+    {
+      fetchState: 0,
+      trucks: [],
+      count: 0,
+    }
+  );
+
+  // useEffect(() => {
+  //   if (!map) return;
+  // }, []);
+
+  const trackGarbageTruck = () => {
+    if (trucks["fetchState"] == 1) {
+      const truck = trucks["trucks"][0];
+
+      if (route) {
+        map.removeControl(route);
+        setRoute(null);
+      }
+
+      if (marker.length > 0) {
+        marker.map((item) => {
+          map.removeLayer(item);
+        });
+
+        setMarker([]);
+      }
+
+      const current = truck.current[truck.current.length - 1]
+        .split(",")
+        .map((i) => parseFloat(i.trim()));
+      var points = [];
+
+      truck.current.map((item) => {
+        try {
+          points.push(item.split(",").map((i) => parseFloat(i.trim())));
+        } catch {
+          return;
+        }
+      });
+
+      map.flyTo(current, 18);
+
+      const temp = L.polyline(points, {
+        color: "red",
+        weight: 5,
+        opacity: 0.6,
+        smoothFactor: 1,
+      });
+
+      const tempM = [points[points.length - 1]].map((item) =>
+        L.marker(item).addTo(map)
+      );
+
+      temp.addTo(map);
+      setMarker(tempM);
+      setRoute(temp);
+    }
+  };
+
+  useEffect(() => {
+    if (!map) return;
+    trackGarbageTruck();
+  }, [trucks["trucks"]]);
+
+  useEffect(() => {
+    const query = ref(rdb, "/NODES");
+    return onValue(query, (snapshot) => {
+      const data = snapshot.val();
+
+      if (snapshot.exists()) {
+        const trucks = Object.keys(data).map((key) => data[key]);
+
+        setTrucks({
+          fetchState: 1,
+          trucks: trucks,
+          count: trucks.length,
+        });
+
+        return;
+      }
+
+      setTrucks({
+        fetchState: 2,
+      });
+    });
+  }, []);
+
   return (
     <div className="w-full h-full rounded-lg">
-      <TrackerMap map={map} setMap={setMap} />
+      <TrackerMap
+        map={map}
+        setMap={setMap}
+        trucks={trucks}
+        track={() => {
+          trackGarbageTruck();
+        }}
+      />
     </div>
   );
 }
--- src/components/TrackerMap.jsx
+++ src/components/TrackerMap.jsx
@@ -10,19 +10,25 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { GpsFixed } from "@mui/icons-material";
+import { CircularProgress } from "@mui/material";
 
-function TrackerMap({ map, setMap }) {
+function TrackerMap({ map, setMap, trucks, track }) {
   const initialMap = useMemo(
     () => (
       <MapContainer
         ref={setMap}
         className="h-full z-0"
-        center={[51.505, -0.09]}
+        center={[8.054375, 125.195331]}
         zoom={13}
+        rotate={true}
+        touchRotate={true}
+        rotateControl={{
+          closeOnZeroBearing: false,
+        }}
       >
         <TileLayer
           maxZoom={19.8}
-          minZoom={18}
+          minZoom={13}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
@@ -33,7 +39,20 @@ function TrackerMap({ map, setMap }) {
 
   return (
     <div className="relative w-full h-full overflow-hidden rounded-lg">
-      <button className="absolute z-10 w-[200px] flex flex-row items-center h-11 bg-[#19AF0C]/90 bottom-8 right-4 rounded-lg justify-center gap-2">
+      {trucks.fetchState == 0 && (
+        <div className="w-full h-full absolute bg-white/20 text-[#2f2f2f] z-10 gap-2 flex flex-col items-center justify-center">
+          <CircularProgress color="inherit" size={"32px"} />
+          <h1 className="font-arimo font-bold text-sm">
+            Loading, please wait...
+          </h1>
+        </div>
+      )}
+      <button
+        onClick={() => {
+          track();
+        }}
+        className="absolute z-10 w-[200px] flex flex-row items-center h-11 bg-[#19AF0C]/90 bottom-8 right-4 rounded-lg justify-center gap-2"
+      >
         <GpsFixed fontSize="small" />
         <p className="text-sm font-inter-bold">Track Garbage Truck</p>
       </button>
